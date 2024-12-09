8914079d42f78676d7f7709b3a2c9d36c2435baf
Ivo Jérôme Hartwig <i.hartwig@hbk-bs.de>
2024-12-09 14:40:46 +0100

    speed changes
    speed changes

--- p5/circular pendel 2/index.js
+++ p5/circular pendel 2/index.js
@@ -1,11 +1,14 @@
 let centerX, centerY; // Mittelpunkt des Kreises
 let radiustacho; // Radien der Kreise
-let radiustach2;
+let radiustacho2;
+let radiustacho3;
 let angle = 0;  
-let angle2 =0;     // Aktueller Winkel für Mitte
+let angle2 =0;   
+let angle3 =0;  // Aktueller Winkel für Mitte
       
 let speed = 0.01;
 let speed2 = 0.02;
+let speed3 = 0.03;
 
 let clickCount = 0;
 
@@ -22,8 +25,10 @@ function setup() {
 
   // Setze Radien relativ zur Leinwandgröße
  
-  radiustacho = width * 0.2;  // 30% der Breite
-  radiustacho2 = width * 0.4
+  radiustacho = width * 0.15;  // 30% der Breite
+  radiustacho2 = width * 0.3;
+  radiustacho3 = width * 0.45
+
  
 
   centerX = width / 2; // Zentrum des Kreises in der Mitte der Canvas
@@ -47,11 +52,20 @@ function draw() {
   angle2 += speed2
   stroke(color1)
   line(x1,y1,x2,y2)
+
+  let x3 = centerX + radiustacho3 * cos(angle3);
+  let y3 = centerY + radiustacho3 * sin(angle3);
+  angle3 += speed3
+  stroke(color1)
+  line(x2,y2,x3,y3)
   
+
+
 }
 
 function mousePressed(){
   clickCount++
   speed = clickCount % 1 === 1 ? speed : speed* -1;
   speed2 = clickCount % 1 === 1 ? speed2 : speed2* -1;
+  speed3 = clickCount % 1 === 1 ? speed3 : speed3* -1;
 }
