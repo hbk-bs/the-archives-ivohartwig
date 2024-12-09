fe1876002960118f7033e3124dc2183f002a102f
Ivo Jérôme Hartwig <i.hartwig@hbk-bs.de>
2024-12-09 20:37:39 +0100

    ordnung

---
+++ p5/circular/circular pendel class/index.js
@@ -0,0 +1,102 @@
+class Segmentor {
+  centerX
+  centerY;
+  angle;
+  speed;
+  positions = []
+  segmentCount;
+  constructor({ x, y, segmentCount = 8, speed = 0.1 }) {
+    this.centerX = x;
+    this.centerY = y;
+    this.speed = speed;
+    this.angle = 0;
+    this.segmentCount = segmentCount;
+  }
+  update() {
+
+    let radius = 0;
+    this.positions = []
+    for (let i = 0; i < this.segmentCount; i++) {
+      const x = radius * cos(this.angle * i)
+      const y = radius * sin(this.angle * i)
+      this.positions.push({ x, y })
+      radius += width * 0.03;
+      this.angle += this.speed;
+      // this.speed += 0.01;
+      console.log(this.angle)
+    }
+
+    // if(this.speed > 0.05){
+    //   this.speed = 0.05
+    // }
+  }
+  display() {
+    push()
+    noFill()
+    stroke(128)
+    translate(this.centerX, this.centerY)
+    for (let i = 0; i < this.positions.length; i++) {
+      if (i === 0) {
+        line(0, 0, this.positions[i].x, this.positions[i].y);
+      } else {
+        line(this.positions[i - 1].x, this.positions[i - 1].y, this.positions[i].x, this.positions[i].y);
+
+      }
+      fill(128)
+      circle(this.positions[i].x, this.positions[i].y, 4)
+    }
+    pop()
+
+  }
+}
+
+let linethings = [];
+function setup() {
+  createCanvas(800, 400);
+  angleMode(DEGREES)
+  linethings.push(new Segmentor({
+    x: width * 0.1,
+    y: height / 2,
+    speed: 0.2,
+    segmentCount: 2
+  }))
+
+  linethings.push(new Segmentor({
+    x: width * 0.3,
+    y: height / 2,
+    speed: 0.1,
+    segmentCount: 4
+  }))
+
+  linethings.push(new Segmentor({
+    x: width * 0.5,
+    y: height / 2,
+    speed: 0.0667,
+    segmentCount: 6
+  }))
+
+  linethings.push(new Segmentor({
+    x: width * 0.7,
+    y: height / 2,
+    speed: 0.05,
+    segmentCount: 8
+  }))
+  
+  
+}
+
+function draw() {
+  background(0);
+  linethings.forEach((element) => {
+    element.display();
+    // if (frameCount % 50 === 0) {
+    element.update()
+    // }
+  })
+}
+
+function mousePressed() {
+  linethings.forEach((segmentor) => {
+    segmentor.speed *= -1; // Invertiert die Geschwindigkeit jedes Segmentors
+  });
+}
--- p5/circular/circular pendel extrem/index.js
+++ p5/circular/circular pendel extrem/index.js
@@ -5,7 +5,7 @@ class Segmentor {
   speed;
   positions = []
   segmentCount;
-  constructor({ x, y, segmentCount = 8, speed = 0.1 }) {
+  constructor({ x, y, segmentCount = 50, speed = 0.1 }) {
     this.centerX = x;
     this.centerY = y;
     this.speed = speed;
@@ -20,7 +20,7 @@ class Segmentor {
       const x = radius * cos(this.angle * i)
       const y = radius * sin(this.angle * i)
       this.positions.push({ x, y })
-      radius += width * 0.05;
+      radius += width * 0.007;
       this.angle += this.speed;
       // this.speed += 0.01;
       console.log(this.angle)
@@ -42,7 +42,8 @@ class Segmentor {
         line(this.positions[i - 1].x, this.positions[i - 1].y, this.positions[i].x, this.positions[i].y);
 
       }
-      circle(this.positions[i].x, this.positions[i].y, 10)
+      fill(128)
+      //circle(this.positions[i].x, this.positions[i].y, 4)
     }
     pop()
 
@@ -56,24 +57,13 @@ function setup() {
   linethings.push(new Segmentor({
     x: width / 2,
     y: height / 2,
-    speed: 0.01,
-    segmentCount: 10
+    speed: 0.002,
+    segmentCount: 50
   }))
 
-  linethings.push(new Segmentor({
-    x: width / 4,
-    y: height / 2,
-    speed: 0.1,
-    segmentCount: 5
-  }))
-
-  linethings.push(new Segmentor({
-    x: (width / 4 )* 3,
-    y: height / 4,
-    speed: 0.02,
-    segmentCount: 6
-  }))
-  // linethings.push(new Segmentor(width / 8, height / 2))
+ 
+  
+  
 }
 
 function draw() {
@@ -84,4 +74,10 @@ function draw() {
     element.update()
     // }
   })
+}
+
+function mousePressed() {
+  linethings.forEach((segmentor) => {
+    segmentor.speed *= -1; // Invertiert die Geschwindigkeit jedes Segmentors
+  });
 }
