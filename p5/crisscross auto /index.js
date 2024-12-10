8bbe61f7b9697d1776a4a1e1a73825ea313dfb22
Ivo Jérôme Hartwig <i.hartwig@hbk-bs.de>
2024-12-10 14:42:03 +0100

    bla

--- p5/crisscross auto /index.js
+++ p5/crisscross auto /index.js
@@ -4,10 +4,18 @@ let y = 0;
 let x1 = 0;
 let y1 = 0;
 
+let x2 = 0;
+let y2 = 0;
+
+let x3 = 0;
+let y3 = 0;
+
 let radius;
 
 let speed1 = 1;
 let speed2 = 1;
+let speed3 = 1;
+let speed4 = 1;
 
 var numbers = [];
 
@@ -25,9 +33,13 @@ function preload() {
 function setup() {
   const canvas = createCanvas(400, 400);
   canvas.parent("sketch");
-  y = 0;
-  x1 = width +width*0.01 ; 
-  y1 = width*0.075 ; 
+
+  x= width*0.075 
+  x1 = -width +width*0.075 ; 
+  y2 =  height -height*0.075 ; 
+  x3 = width
+  y3 = height + height*0.075
+   
   radius = width/10
   
   strokeWeight(0);
@@ -60,6 +72,8 @@ function draw() {
   );
   pop();
 
+  quad(X(0.5),Y(0.4),X(0.6),Y(0.5),X(0.5),Y(0.6),X(0.4),Y(0.5))
+
   stroke("white")
 strokeWeight(2)
 push()
@@ -68,8 +82,8 @@ rotate(QUARTER_PI)
 let Y1 = -height
 for (let i = 0; i < 50; i++) {
   const X1 = width * 0.71
-  const Y2 = Y1 + 15
-  Y1 += 20
+  const Y2 = Y1 + 20
+  Y1 += 30
   line(X1, Y1, X1, Y2)
 }
 pop()
@@ -82,45 +96,73 @@ rotate(-QUARTER_PI)
 let Y11 = -height
 for (let i = 0; i < 50; i++) {
   const X2 = width * 0.71
-  const Y22 = Y11 + 15
-  Y11 += 20
+  const Y22 = Y11 + 20
+  Y11 += 30
   line(X2, Y11, X2, Y22)
 }
 pop()
 
-  
+fill("grey")
+strokeWeight(0)
+quad(X(0.5),Y(0.36),X(0.64),Y(0.5),X(0.5),Y(0.64),X(0.36),Y(0.5))
 
   
 
   
 
-
-
-  // Erster Kreis: horizontal und vertikal
-  imageMode(CENTER); // Setzt den Modus, sodass das Bild von der Mitte aus gezeichnet wird
-  image(img1, x, y, width / 10, height / 10); // Zeichnet das Bild in der Mitte
-  speed1 = 1.5;
-  x += speed1; //nach rechts
-  y += speed1; //nach unten
-  console.log(x, y);
-  if (x > width + radius) {
-    x = -radius+ width*0.075;
-    y = -radius;
-    speed1 = 1;
-  }
   
 
-  // Zweiter Kreis: diagonal von rechts oben nach links unten
-  imageMode(CENTER); // Setzt den Modus, sodass das Bild von der Mitte aus gezeichnet wird
-  image(img2, x1, y1, width / 10, height / 10); // Zeichnet das Bild in der Mitte
-  speed2 *= 1.0;
-  x1 -= speed2; //nach links
-  y1 += speed2; //nach unten
-  console.log(x1, y1);
-  if (y1 > height + radius || x1 < -radius) {
-    x1 = width+radius; 
-    y1 = -radius + width*0.075 ; 
-    speed2 = 1;
-  }
+
+
+imageMode(CENTER); // Setzt den Modus, sodass das Bild von der Mitte aus gezeichnet wird
+image(img1, x, y, width / 10, height / 10); // Zeichnet das Bild in der Mitte
+speed1 = 1;
+x += speed1; //nach rechts
+y += speed1; //nach unten
+console.log(x, y);
+if (x > width + radius) {
+  x = -radius+ width*0.075;
+  y = -radius;
+  speed1 = 1;
+}
+
+
+// Zweiter Kreis: diagonal von rechts oben nach links unten
+imageMode(CENTER); // Setzt den Modus, sodass das Bild von der Mitte aus gezeichnet wird
+image(img2, x1, y1, width / 10, height / 10); // Zeichnet das Bild in der Mitte
+speed2 *= 1.0;
+x1 -= speed2; //nach links
+y1 += speed2; //nach unten
+console.log(x1, y1);
+if (y1 > height + radius || x1 < -radius) {
+  x1 = width+radius; 
+  y1 = -radius + width*0.075 ; 
+  speed2 = 1;
+}
+
+imageMode(CENTER); // Setzt den Modus, sodass das Bild von der Mitte aus gezeichnet wird
+image(img2, x2, y2, width / 10, height / 10); // Zeichnet das Bild in der Mitte
+speed1 = 1.5;
+x2 += speed3; //nach rechts
+y2 -= speed3; //nach unten
+console.log(x, y);
+if (x2 > width + radius) {
+  x2 = -radius
+  y2 =  height+radius-width*0.075;
+  speed3 = 1;
+}
+
+imageMode(CENTER); // Setzt den Modus, sodass das Bild von der Mitte aus gezeichnet wird
+image(img1, x3, y3, width / 10, height / 10); // Zeichnet das Bild in der Mitte
+speed2 *= 1.0;
+x3 -= speed4; //nach links
+y3 -= speed4; //nach unten
+console.log(x1, y1);
+if (x3 < -radius || y3 < -radius) {
+  x3 = width + radius;
+  y3 = height + radius + height * 0.075; // Rücksetzen wie die anderen Autos
+  speed4= 1
+}
+
   
 }
